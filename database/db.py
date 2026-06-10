a39928ea380e77e0bb108c8065fea25fc1d759e9
akashravuru <ravuruakash@gmail.com>
2026-06-10 12:25:37 +0530

    Fix database initialization for deployment

--- database/db.py
+++ database/db.py
@@ -1,9 +1,14 @@
+import os
 import sqlite3
+
 from database.schema import create_tables
 from config import DB_PATH
 
 
 def init_db():
+
+    os.makedirs("data", exist_ok=True)
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
