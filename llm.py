bfee60fbbfc02fb0a26f144db07060e007fbeac4
akashravuru <ravuruakash@gmail.com>
2026-06-10 12:20:19 +0530

    Add Streamlit secrets support

--- llm.py
+++ llm.py
@@ -1,11 +1,19 @@
 import os
+import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GEMINI_API_KEY"]
+
 genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
+    api_key=api_key
 )
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel(
+    "gemini-2.5-flash"
+)
